75c1dd84e04420188f39fabaabe9ab17bbf7e36b
Timóteo Stifft <timoteostifft@gmail.com>
2022-01-03 11:09:52 -0300

    feature: mark todo as done

--- src/index.js
+++ src/index.js
@@ -87,7 +87,14 @@ app.put("/todos/:id", checksExistsUserAccount, (request, response) => {
 });
 
 app.patch("/todos/:id/done", checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { id } = request.params;
+
+  const todo = user.todos.find((todo) => (todo.id = id));
+
+  todo.done = true;
+
+  return response.json(todo);
 });
 
 app.delete("/todos/:id", checksExistsUserAccount, (request, response) => {
