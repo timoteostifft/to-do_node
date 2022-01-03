302ce761ff7c347b72de3bf78122b8b8d7e96032
Timóteo Stifft <timoteostifft@gmail.com>
2022-01-03 11:18:44 -0300

    feature: delete todo

--- src/index.js
+++ src/index.js
@@ -92,13 +92,28 @@ app.patch("/todos/:id/done", checksExistsUserAccount, (request, response) => {
 
   const todo = user.todos.find((todo) => (todo.id = id));
 
+  if (!todo) {
+    return response.status(404).json({ error: "Todo not found" });
+  }
+
   todo.done = true;
 
   return response.json(todo);
 });
 
 app.delete("/todos/:id", checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { id } = request.params;
+
+  const todoIndex = user.todos.findIndex((todo) => todo.id === id);
+
+  if (todoIndex === -1) {
+    return response.status(404).json({ error: "Todo not found" });
+  }
+
+  user.todos.splice(todoIndex, 1);
+
+  return response.status(204).json();
 });
 
 module.exports = app;
